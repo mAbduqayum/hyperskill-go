dc11074b80b5523e05a1a054fbb8c75301eb9919
Muhammad-Ali <mabduqayum@transparent.com>
2024-02-08 12:45:49 +0500

    solver 15th

--- 15 air conditioner/air_conditioner.go
+++ 15 air conditioner/air_conditioner.go
@@ -7,16 +7,34 @@ import (
 )
 
 func main() {
-	var in *bufio.Reader
-	var out *bufio.Writer
-	in = bufio.NewReader(os.Stdin)
-	out = bufio.NewWriter(os.Stdout)
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
 
-	var t int
-	fmt.Fscan(in, &t)
-	var line string
+	var t, count, bound int
+	var sign string
+	fmt.Fscanln(in, &t)
 	for i := 0; i < t; i++ {
-		fmt.Fscan(in, &line)
+		fmt.Fscanln(in, &count)
+		lBound := 15
+		uBound := 30
+		valid := true
+		for j := 0; j < count; j++ {
+			fmt.Fscanln(in, &sign, &bound)
+			if sign == ">=" && bound > lBound {
+				lBound = bound
+			} else if sign == "<=" && bound < uBound {
+				uBound = bound
+			}
+			if lBound > uBound {
+				valid = false
+			}
+			if !valid {
+				fmt.Fprintln(out, -1)
+			} else {
+				fmt.Fprintln(out, lBound)
+			}
+		}
+		fmt.Fprintln(out)
 	}
 }
