540227eb5dd1dc0f3c6df48d218281674762de9e
mAbduqayum <mabduqayum@gmail.com>
2024-02-12 02:44:50 +0500

    refactored

--- 26_good_marks/good_marks.go
+++ 26_good_marks/good_marks.go
@@ -49,28 +49,55 @@ func findIJ(n int, m int, grid [][]int) {
 		}
 	}
 	worst := gridSmallest(rowSummary, colSummary)
-	r := smallestIndex(rowSummary...)
-	c := smallestIndex(colSummary...)
+	smallestRow, rs := smallestIndexes(rowSummary...)
+	smallestCol, cs := smallestIndexes(colSummary...)
+	r := rs[0]
+	c := cs[0]
 
 	x, y := -1, -1
+	if len(rs) == 1 && len(cs) == 1 {
+		//// strike smallestRow against one of smallestCol and check if it's better
+		//newColSummary := append(colSummary[:c], colSummary[c+1:]...)
+		//for j := 0; j < m; j++ {
+		//	w := gridSmallestWithoutIJ(newColSummary, colSummary, r, j)
+		//	if w > worst {
+		//		worst = w
+		//		x, y = smallestRow, j
+		//	}
+		//}
+		//
+		//// strike smallestCol against one of smallestRow and check if it's better
+		//newRowSummary := append(rowSummary[:r], rowSummary[r+1:]...)
+		//for i := 0; i < n; i++ {
+		//	w := gridSmallestWithoutIJ(newRowSummary, colSummary, i, c)
+		//	if w > worst {
+		//		worst = w
+		//		x, y = i, smallestCol
+		//	}
+		//}
+	} else if len(rs) == 1 {
 
-	// strike r against one of c and check if it's better
-	for j := 0; j < m; j++ {
-		w := gridSmallestWithoutIJ(rowSummary, colSummary, r, j)
-		if w > worst {
-			worst = w
-			x, y = r, j
+		var newColSummary []int
+		for j := 0; j < m; j++ {
+			if colSummary[j] == smallestCol {
+				continue
+			}
+			newColSummary = append(newColSummary, colSummary[j])
 		}
+		newRowSummary := append(rowSummary[:r], rowSummary[r+1:]...)
+		for i := 0; i < n; i++ {
+			wRow := smallestStrikeI(newRowSummary, i)
+			wCol := smallest(newColSummary...)
+			w := smallest(wRow, wCol)
+			if w > worst {
+				worst = w
+				x, y = i, c
+			}
+		}
+	} else if len(cs) == 1 {
 	}
 
-	// strike c against one of r and check if it's better
-	for i := 0; i < n; i++ {
-		w := gridSmallestWithoutIJ(rowSummary, colSummary, i, c)
-		if w > worst {
-			worst = w
-			x, y = i, c
-		}
-	}
+	fmt.Fprintln(out, smallestRow)
 
 	fmt.Fprintln(out, x+1, y+1)
 }
@@ -85,7 +112,7 @@ func smallest(nums ...int) int {
 	return rez
 }
 
-func smallestAndIndexes(nums ...int) (int, []int) {
+func smallestIndexes(nums ...int) (int, []int) {
 	rez := nums[0]
 	indexes := []int{0}
 	for i, num := range nums {
@@ -109,7 +136,7 @@ func smallestIndex(nums ...int) int {
 	return rez
 }
 
-func smallestWithoutI(summary []int, index int) int {
+func smallestStrikeI(summary []int, index int) int {
 	rez := 5
 	for i, num := range summary {
 		if i != index && num < rez {
@@ -122,13 +149,11 @@ func smallestWithoutI(summary []int, index int) int {
 func gridSmallest(rowSummary, colSummary []int) int {
 	rowSmallest := smallest(rowSummary...)
 	colSmallest := smallest(colSummary...)
-	rez := smallest(rowSmallest, colSmallest)
-	return rez
+	return smallest(rowSmallest, colSmallest)
 }
 
-func gridSmallestWithoutIJ(rowSummary, colSummary []int, i, j int) int {
-	rowSmallest := smallestWithoutI(rowSummary, j)
-	colSmallest := smallestWithoutI(colSummary, i)
-	rez := smallest(rowSmallest, colSmallest)
-	return rez
-}
+//func gridSmallestWithoutIJ(rowSummary, colSummary []int, i, j int) int {
+//	rowSmallest := smallestStrikeI(rowSummary, j)
+//	colSmallest := smallestStrikeI(colSummary, i)
+//	return smallest(rowSmallest, colSmallest)
+//}
