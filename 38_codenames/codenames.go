f5d1573357e945170abb6109bf1372d14adb7262
mAbduqayum <mAbduqayum@gmail.com>
2024-04-26 19:41:14 +0500

    add 38

---
+++ 38_codenames/codenames.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer out.Flush()
+	var t int
+	fmt.Fscan(in, &t)
+	for i := 0; i < t; i++ {
+		processTest()
+	}
+}
+
+func processTest() {
+	//var n, m int
+	//fmt.Fscan(in, &slots[i])
+}
