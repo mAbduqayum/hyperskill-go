d6139d14f6aa3ad8a086bc13c566a4dd85dcc465
mAbduqayum <mabduqayum@gmail.com>
2024-08-27 11:50:44 +0500

    Stage 3

--- 01_nn_hyperskill/12_vcs/vcs.go
+++ 01_nn_hyperskill/12_vcs/vcs.go
@@ -1,53 +1,93 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+func main() {
+	vcs := NewVCS()
+
+	if len(os.Args) < 2 || os.Args[1] == "--help" {
+		vcs.PrintHelp()
+		return
+	}
+
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	switch command {
+	case "config":
+		vcs.HandleConfig(args)
+	case "add":
+		vcs.HandleAdd(args)
+	case "commit":
+		vcs.HandleCommit(args)
+	case "log":
+		vcs.HandleLog()
+	default:
+		if !vcs.HandleUnknownCommand(command) {
+			fmt.Printf("'%s' is not a SVCS command.\n", command)
+		}
+	}
+}
+
 const (
 	vcsDir     = "vcs"
 	configFile = "config.txt"
 	indexFile  = "index.txt"
+	logFile    = "log.txt"
+	commitsDir = "commits"
 )
 
-var commands = []struct {
-	name string
-	desc string
-}{
-	{"config", "Get and set a username."},
-	{"add", "Add a file to the index."},
-	{"log", "Show commit logs."},
-	{"commit", "Save changes."},
-	{"checkout", "Restore a file."},
+type Command struct {
+	Name string
+	Desc string
 }
 
-func initVCS() {
-	err := os.MkdirAll(vcsDir, os.ModePerm)
-	if err != nil {
+type VCS struct {
+	Commands []Command
+}
+
+func NewVCS() *VCS {
+	vcs := &VCS{
+		Commands: []Command{
+			{"config", "Get and set a username."},
+			{"add", "Add a file to the index."},
+			{"log", "Show commit logs."},
+			{"commit", "Save changes."},
+			{"checkout", "Restore a file."},
+		},
+	}
+	vcs.initVCS()
+	return vcs
+}
+
+func (v *VCS) initVCS() {
+	if err := os.MkdirAll(vcsDir, os.ModePerm); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func readFile(filename string) string {
-	data, err := ioutil.ReadFile(filepath.Join(vcsDir, filename))
+func (v *VCS) readFile(filename string) string {
+	data, err := os.ReadFile(filepath.Join(vcsDir, filename))
 	if err != nil && !os.IsNotExist(err) {
 		log.Fatal(err)
 	}
 	return strings.TrimSpace(string(data))
 }
 
-func writeFile(filename, content string) error {
-	return ioutil.WriteFile(filepath.Join(vcsDir, filename), []byte(content), 0644)
+func (v *VCS) writeFile(filename, content string) error {
+	return os.WriteFile(filepath.Join(vcsDir, filename), []byte(content), 0644)
 }
 
-func handleConfig(args []string) {
+func (v *VCS) HandleConfig(args []string) {
 	if len(args) == 0 {
-		username := readFile(configFile)
+		username := v.readFile(configFile)
 		if username == "" {
 			fmt.Println("Please, tell me who you are.")
 		} else {
@@ -55,17 +95,16 @@ func handleConfig(args []string) {
 		}
 	} else {
 		username := strings.Join(args, " ")
-		err := writeFile(configFile, username)
-		if err != nil {
+		if err := v.writeFile(configFile, username); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("The username is %s.\n", username)
 	}
 }
 
-func handleAdd(args []string) {
+func (v *VCS) HandleAdd(args []string) {
 	if len(args) == 0 {
-		trackedFiles := readFile(indexFile)
+		trackedFiles := v.readFile(indexFile)
 		if trackedFiles == "" {
 			fmt.Println("Add a file to the index.")
 		} else {
@@ -79,14 +118,13 @@ func handleAdd(args []string) {
 			return
 		}
 
-		trackedFiles := readFile(indexFile)
+		trackedFiles := v.readFile(indexFile)
 		if !strings.Contains(trackedFiles, filename) {
 			if trackedFiles != "" {
 				trackedFiles += "\n"
 			}
 			trackedFiles += filename
-			err := writeFile(indexFile, trackedFiles)
-			if err != nil {
+			if err := v.writeFile(indexFile, trackedFiles); err != nil {
 				log.Fatal(err)
 			}
 		}
@@ -94,40 +132,123 @@ func handleAdd(args []string) {
 	}
 }
 
-func printHelp() {
-	fmt.Println("These are SVCS commands:")
-	for _, cmd := range commands {
-		fmt.Printf("%-10s %s\n", cmd.name, cmd.desc)
-	}
-}
-
-func main() {
-	initVCS()
-
-	if len(os.Args) < 2 || os.Args[1] == "--help" {
-		printHelp()
+func (v *VCS) HandleCommit(args []string) {
+	if len(args) == 0 {
+		fmt.Println("Message was not passed.")
 		return
 	}
 
-	command := os.Args[1]
-	args := os.Args[2:]
+	message := strings.Join(args, " ")
+	trackedFiles := strings.Split(v.readFile(indexFile), "\n")
+	if len(trackedFiles) == 0 || (len(trackedFiles) == 1 && trackedFiles[0] == "") {
+		fmt.Println("Nothing to commit.")
+		return
+	}
 
-	switch command {
-	case "config":
-		handleConfig(args)
-	case "add":
-		handleAdd(args)
-	default:
-		found := false
-		for _, cmd := range commands {
-			if cmd.name == command {
-				fmt.Println(cmd.desc)
-				found = true
-				break
-			}
+	lastCommitHash := v.getLastCommitHash()
+	currentHash := v.calculateFilesHash(trackedFiles)
+
+	if currentHash == lastCommitHash {
+		fmt.Println("Nothing to commit.")
+		return
+	}
+
+	commitDir := filepath.Join(vcsDir, commitsDir, currentHash)
+	if err := os.MkdirAll(commitDir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range trackedFiles {
+		if file == "" {
+			continue
 		}
-		if !found {
-			fmt.Printf("'%s' is not a SVCS command.\n", command)
+		content, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := os.WriteFile(filepath.Join(commitDir, filepath.Base(file)), content, 0644); err != nil {
+			log.Fatal(err)
 		}
 	}
+
+	username := v.readFile(configFile)
+	logEntry := fmt.Sprintf("commit %s\nAuthor: %s\n%s", currentHash, username, message)
+	v.prependToFile(filepath.Join(vcsDir, logFile), logEntry)
+
+	fmt.Println("Changes are committed.")
+}
+
+func (v *VCS) HandleLog() {
+	logContent := v.readFile(logFile)
+	if logContent == "" {
+		fmt.Println("No commits yet.")
+	} else {
+		commits := strings.Split(strings.TrimSpace(logContent), "\n\n")
+		for i := len(commits) - 1; i >= 0; i-- {
+			fmt.Println(commits[i])
+			if i > 0 {
+				fmt.Println()
+			}
+		}
+	}
+}
+
+func (v *VCS) getLastCommitHash() string {
+	entries, err := os.ReadDir(filepath.Join(vcsDir, commitsDir))
+	if err != nil {
+		return ""
+	}
+	if len(entries) == 0 {
+		return ""
+	}
+	return entries[len(entries)-1].Name()
+}
+
+func (v *VCS) calculateFilesHash(files []string) string {
+	h := sha256.New()
+	for _, file := range files {
+		if file == "" {
+			continue
+		}
+		content, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		h.Write(content)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func (v *VCS) prependToFile(filename, content string) {
+	existingContent, err := os.ReadFile(filename)
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	newContent := []byte(content)
+	if len(existingContent) > 0 {
+		newContent = append(newContent, '\n')
+		newContent = append(newContent, existingContent...)
+	}
+
+	if err := os.WriteFile(filename, newContent, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (v *VCS) PrintHelp() {
+	fmt.Println("These are SVCS commands:")
+	for _, cmd := range v.Commands {
+		fmt.Printf("%-10s %s\n", cmd.Name, cmd.Desc)
+	}
+}
+
+func (v *VCS) HandleUnknownCommand(command string) bool {
+	for _, cmd := range v.Commands {
+		if cmd.Name == command {
+			fmt.Println(cmd.Desc)
+			return true
+		}
+	}
+	return false
 }
