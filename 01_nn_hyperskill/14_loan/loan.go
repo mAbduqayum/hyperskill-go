3464d6a45adc27d882f688912a91a2fa10aae6f5
mAbduqayum <mabduqayum@gmail.com>
2024-08-27 12:39:02 +0500

    Refactor

--- 01_nn_hyperskill/14_loan/loan.go
+++ 01_nn_hyperskill/14_loan/loan.go
@@ -7,115 +7,125 @@ import (
 	"os"
 )
 
+type LoanCalculator struct {
+	PaymentType string
+	Payment     float64
+	Principal   float64
+	Periods     int
+	Interest    float64
+}
+
 func main() {
-	var paymentType string
-	var payment, principal, interest float64
-	var periods int
+	lc := parseFlags()
 
-	flag.StringVar(&paymentType, "type", "", "type of payment: 'annuity' or 'diff'")
-	flag.Float64Var(&payment, "payment", 0, "the monthly payment amount")
-	flag.Float64Var(&principal, "principal", 0, "the loan principal amount")
-	flag.IntVar(&periods, "periods", 0, "the number of months needed to repay the loan")
-	flag.Float64Var(&interest, "interest", 0, "the loan interest")
-
-	flag.Parse()
-
-	if !validateInput(paymentType, payment, principal, periods, interest) {
+	if !lc.validateInput() {
 		fmt.Println("Incorrect parameters")
 		os.Exit(1)
 	}
 
-	monthlyInterest := interest / (12 * 100)
+	monthlyInterest := lc.Interest / (12 * 100)
 
-	switch paymentType {
+	switch lc.PaymentType {
 	case "diff":
-		if payment != 0 {
+		if lc.Payment != 0 {
 			fmt.Println("Incorrect parameters")
 			os.Exit(1)
 		}
-		calculateDiff(principal, periods, monthlyInterest)
+		lc.calculateDiff(monthlyInterest)
 	case "annuity":
-		calculateAnnuity(payment, principal, periods, monthlyInterest)
+		lc.calculateAnnuity(monthlyInterest)
 	default:
 		fmt.Println("Incorrect parameters")
 		os.Exit(1)
 	}
 }
 
-func validateInput(paymentType string, payment, principal float64, periods int, interest float64) bool {
-	if paymentType == "" || interest == 0 {
+func parseFlags() *LoanCalculator {
+	lc := &LoanCalculator{}
+	flag.StringVar(&lc.PaymentType, "type", "", "type of payment: 'annuity' or 'diff'")
+	flag.Float64Var(&lc.Payment, "payment", 0, "the monthly payment amount")
+	flag.Float64Var(&lc.Principal, "principal", 0, "the loan principal amount")
+	flag.IntVar(&lc.Periods, "periods", 0, "the number of months needed to repay the loan")
+	flag.Float64Var(&lc.Interest, "interest", 0, "the loan interest")
+	flag.Parse()
+	return lc
+}
+
+func (lc *LoanCalculator) validateInput() bool {
+	if lc.PaymentType == "" || lc.Interest == 0 {
 		return false
 	}
-	if payment < 0 || principal < 0 || periods < 0 || interest < 0 {
+	if lc.Payment < 0 || lc.Principal < 0 || lc.Periods < 0 || lc.Interest < 0 {
 		return false
 	}
 	paramCount := 0
-	if payment != 0 {
+	if lc.Payment != 0 {
 		paramCount++
 	}
-	if principal != 0 {
+	if lc.Principal != 0 {
 		paramCount++
 	}
-	if periods != 0 {
+	if lc.Periods != 0 {
 		paramCount++
 	}
-	return paramCount >= 2 // Changed from 3 to 2
+	return paramCount >= 2
 }
 
-func calculateDiff(principal float64, periods int, interest float64) {
+func (lc *LoanCalculator) calculateDiff(interest float64) {
 	var totalPayment float64
-	for m := 1; m <= periods; m++ {
-		payment := (principal / float64(periods)) + (interest * (principal - ((principal * float64(m-1)) / float64(periods))))
+	for m := 1; m <= lc.Periods; m++ {
+		payment := (lc.Principal / float64(lc.Periods)) + (interest * (lc.Principal - ((lc.Principal * float64(m-1)) / float64(lc.Periods))))
 		roundedPayment := math.Ceil(payment)
 		totalPayment += roundedPayment
 		fmt.Printf("Month %d: payment is %d\n", m, int(roundedPayment))
 	}
-	overpayment := totalPayment - principal
+	overpayment := totalPayment - lc.Principal
 	fmt.Printf("\nOverpayment = %d\n", int(overpayment))
 }
 
-func calculatePayment(principal float64, periods int, interest float64) float64 {
-	return principal * (interest * math.Pow(1+interest, float64(periods))) / (math.Pow(1+interest, float64(periods)) - 1)
-}
-
-func calculateAnnuity(payment, principal float64, periods int, interest float64) {
-	if payment == 0 {
-		payment = calculatePayment(principal, periods, interest)
-		fmt.Printf("Your annuity payment = %d!\n", int(math.Ceil(payment)))
-	} else if principal == 0 {
-		principal = calculatePrincipal(payment, periods, interest)
-		fmt.Printf("Your loan principal = %d!\n", int(principal))
-	} else if periods == 0 {
-		periods = calculatePeriods(payment, principal, interest)
-		printLoanTerm(periods)
+func (lc *LoanCalculator) calculateAnnuity(interest float64) {
+	if lc.Payment == 0 {
+		lc.Payment = lc.calculatePayment(interest)
+		fmt.Printf("Your annuity payment = %d!\n", int(math.Ceil(lc.Payment)))
+	} else if lc.Principal == 0 {
+		lc.Principal = lc.calculatePrincipal(interest)
+		fmt.Printf("Your loan principal = %d!\n", int(lc.Principal))
+	} else if lc.Periods == 0 {
+		lc.Periods = lc.calculatePeriods(interest)
+		lc.printLoanTerm()
 	}
 
-	totalPayment := math.Ceil(payment) * float64(periods)
-	overpayment := totalPayment - principal
+	totalPayment := math.Ceil(lc.Payment) * float64(lc.Periods)
+	overpayment := totalPayment - lc.Principal
 	fmt.Printf("Overpayment = %d\n", int(overpayment))
 }
 
-func calculatePrincipal(payment float64, periods int, interest float64) float64 {
-	return payment / ((interest * math.Pow(1+interest, float64(periods))) / (math.Pow(1+interest, float64(periods)) - 1))
+func (lc *LoanCalculator) calculatePayment(interest float64) float64 {
+	return lc.Principal * (interest * math.Pow(1+interest, float64(lc.Periods))) / (math.Pow(1+interest, float64(lc.Periods)) - 1)
 }
 
-func calculatePeriods(payment, principal, interest float64) int {
-	return int(math.Ceil(math.Log(payment/(payment-interest*principal)) / math.Log(1+interest)))
+func (lc *LoanCalculator) calculatePrincipal(interest float64) float64 {
+	return lc.Payment / ((interest * math.Pow(1+interest, float64(lc.Periods))) / (math.Pow(1+interest, float64(lc.Periods)) - 1))
 }
 
-func printLoanTerm(periods int) {
-	years := periods / 12
-	months := periods % 12
+func (lc *LoanCalculator) calculatePeriods(interest float64) int {
+	return int(math.Ceil(math.Log(lc.Payment/(lc.Payment-interest*lc.Principal)) / math.Log(1+interest)))
+}
 
-	if years == 0 {
+func (lc *LoanCalculator) printLoanTerm() {
+	years := lc.Periods / 12
+	months := lc.Periods % 12
+
+	switch {
+	case years == 0:
 		fmt.Printf("It will take %d months to repay this loan!\n", months)
-	} else if months == 0 {
+	case months == 0:
 		if years == 1 {
 			fmt.Println("It will take 1 year to repay this loan!")
 		} else {
 			fmt.Printf("It will take %d years to repay this loan!\n", years)
 		}
-	} else {
+	default:
 		if years == 1 {
 			fmt.Printf("It will take 1 year and %d months to repay this loan!\n", months)
 		} else {
